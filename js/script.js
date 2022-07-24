8718d5e61fd151b6ef20bc05fdc4bb82e2d6ebc4
Franco <frcg2424@hotmail.com>
2022-07-24 11:16:28 -0500

    feat: :zap: add call to action section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const input = document.querySelector('#grupo__correo');
+const input_cta = document.querySelector('#input__correo');
 
 const expresiones =
 {
@@ -22,20 +23,36 @@ const validarFormulario = (e)=>
 
 const validarCampo = (expresion,name,input)=>
 {
+    console.log(input.id)
     if(expresion.test(input.value))
     {
         input.classList.remove("wrong-email");
-        document.querySelector(`.input-email p`).classList.remove('invalid-email');
-        document.querySelector(`.input-email p`).classList.add('valid-email');
-        document.querySelector(`.send-email`).removeAttribute("disabled")
+        if(input.id == "input__correo"){
+            document.querySelector(`#input-email p`).classList.remove('invalid-email');
+            document.querySelector(`#input-email p`).classList.add('valid-email');
+            document.querySelector(`.send-email-cta`).removeAttribute("disabled")
+        }
+        else{
+            document.querySelector(`.input-email p`).classList.remove('invalid-email');
+            document.querySelector(`.input-email p`).classList.add('valid-email');
+            document.querySelector(`.send-email`).removeAttribute("disabled")
+        }
         campos[name] = true;
     }
     else
     {
         input.classList.add("wrong-email");
-        document.querySelector(`.input-email p`).classList.add('invalid-email');
-        document.querySelector(`.input-email p`).classList.remove('valid-email');
-        document.querySelector(`.send-email`).setAttribute("disabled", "")
+        if(input.id == "input__correo"){
+            document.querySelector(`#input-email p`).classList.add('invalid-email');
+            document.querySelector(`#input-email p`).classList.remove('valid-email');
+            document.querySelector(`.send-email-cta`).setAttribute("disabled", "")
+        }
+        else{
+            document.querySelector(`input-email p`).classList.add('invalid-email');
+            document.querySelector(`input-email p`).classList.remove('valid-email');
+            document.querySelector(`.send-email`).setAttribute("disabled", "")
+        }
+
         campos[name] = false;
     }
 }
@@ -43,6 +60,9 @@ const validarCampo = (expresion,name,input)=>
 
 input.addEventListener("keyup", validarFormulario);
 input.addEventListener("blur", validarFormulario);
+
+input_cta.addEventListener("keyup", validarFormulario);
+input_cta.addEventListener("blur", validarFormulario);
 
 
 
